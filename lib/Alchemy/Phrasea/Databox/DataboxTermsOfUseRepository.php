31610a279640c4a7548e496c7a29e8f50e65a723
Thibaud Fabre <thibaud@aztech.io>
2016-01-17 14:53:59 +0100

    Bump silex/web-profiler version

---
+++ lib/Alchemy/Phrasea/Databox/DataboxTermsOfUseRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Alchemy\Phrasea\Databox;
+
+class DataboxTermsOfUseRepository
+{
+
+}
---
+++ lib/Alchemy/Phrasea/Databox/Preference/DataboxPreference.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Alchemy\Phrasea\Databox\Preference;
+
+class DataboxPreference
+{
+
+}
